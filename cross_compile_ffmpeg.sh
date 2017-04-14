a7438cd08709ee234f40ea6425fba816ab10f153
Stewart Fraser <stewart.fraser@f-e-t.com>
2017-04-14 10:40:05 +0100

    housekeeping

--- cross_compile_ffmpeg.sh
+++ cross_compile_ffmpeg.sh
@@ -1927,17 +1927,17 @@ if (( libtheora_solution_number == 2 )); then  # run script twice (1) or sed the
   echo
 fi
 echo
-picked_compiler_flavors_file="picked_compiler_flavors.dat"
+current_compiler_flavor_file="current_compiler_flavor.dat"
 yes_no_sel "Do you wish to install the mingw cross-compiler [y/N]?" "n"
 if [[ $user_input == y ]]; then # input "Y"|"y" for yes, "N"|"n" for no, default answer is "n" 
   # install the cross compiler and save its flavor {multi, win32, win64}
   install_cross_compiler 
-  echo "$compiler_flavors" > "$this_shell_script_full_pathname/$picked_compiler_flavors_file"
+  echo "$compiler_flavors" > "$this_shell_script_full_pathname/$current_compiler_flavor_file"
 else
-  if [ -e "$this_shell_script_full_pathname/$picked_compiler_flavors_file" ]; then
-    compiler_flavors=$(<$this_shell_script_full_pathname/$picked_compiler_flavors_file)
+  if [ -e "$this_shell_script_full_pathname/$current_compiler_flavor_file" ]; then
+    compiler_flavors=$(<$this_shell_script_full_pathname/$current_compiler_flavor_file)
   else
-    echo "error: $this_shell_script_full_pathname/$picked_compiler_flavors_file does not exist ... exiting"
+    echo "error: $this_shell_script_full_pathname/$current_compiler_flavor_file does not exist ... exiting"
     exit
   fi
 fi
--- run_ffmpeg_cross_compiler.sh
+++ run_ffmpeg_cross_compiler.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./cross_compile_ffmpeg.sh 2>&1 | tee zzOutputInfoFrom_cross_compile_ffmpeg.sh.dat
+./cross_compile_ffmpeg.sh 2>&1 | tee outputInfoFrom_cross_compile_ffmpeg.sh.dat
