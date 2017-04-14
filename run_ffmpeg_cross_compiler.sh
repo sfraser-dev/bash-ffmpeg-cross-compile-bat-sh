6c5ef5a09ed3ed0ed01c848f85a439ba0f4b561f
Stewart Fraser <stewart.fraser@f-e-t.com>
2017-04-14 17:45:20 +0100

    housekeeping

--- run_ffmpeg_cross_compiler.sh
+++ run_ffmpeg_cross_compiler.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # DLL
-./cross_compile_ffmpeg.sh --build-ffmpeg-static=n --build-ffmpeg-shared=y --enable-gpl=n --disable-nonfree=y --git-get-latest=n --prefer-stable=y --build-intel-qsv=y --build-dvbtee=n --build-mp4box=n --build-mplayer=n --build-vlc=n --build-lsw=n --build-x264-with-libav=n --ffmpeg-git-checkout-version= --build-ismindex=n 2>&1 | tee outputInfoDLLFrom_cross_compile_ffmpeg.sh.dat
+#source ./cross_compile_ffmpeg.sh --build-ffmpeg-static=n --build-ffmpeg-shared=y --enable-gpl=n --disable-nonfree=y --git-get-latest=n --prefer-stable=y --build-intel-qsv=y --build-dvbtee=n --build-mp4box=n --build-mplayer=n --build-vlc=n --build-lsw=n --build-x264-with-libav=n --ffmpeg-git-checkout-version= --build-ismindex=n 2>&1 | tee outputInfoDLLFrom_cross_compile_ffmpeg.sh.dat
 
 # static
-#./cross_compile_ffmpeg.sh --build-ffmpeg-static=y --build-ffmpeg-shared=n --enable-gpl=n --disable-nonfree=y --git-get-latest=n --prefer-stable=y --build-intel-qsv=y --build-dvbtee=n --build-mp4box=n --build-mplayer=n --build-vlc=n --build-lsw=n --build-x264-with-libav=n --ffmpeg-git-checkout-version= --build-ismindex=n 2>&1 | tee outputInfoSTATICFrom_cross_compile_ffmpeg.sh.dat
+#source ./cross_compile_ffmpeg.sh --build-ffmpeg-static=y --build-ffmpeg-shared=n --enable-gpl=n --disable-nonfree=y --git-get-latest=n --prefer-stable=y --build-intel-qsv=y --build-dvbtee=n --build-mp4box=n --build-mplayer=n --build-vlc=n --build-lsw=n --build-x264-with-libav=n --ffmpeg-git-checkout-version= --build-ismindex=n 2>&1 | tee outputInfoSTATICFrom_cross_compile_ffmpeg.sh.dat
