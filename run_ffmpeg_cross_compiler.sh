4444c6011e5e8e30126c27da0858067e63d4ec0e
Stewart Fraser <stewart.fraser@f-e-t.com>
2017-04-19 14:59:43 +0100

    even more informative bin file directory names

--- run_ffmpeg_cross_compiler.sh
+++ run_ffmpeg_cross_compiler.sh
@@ -2,8 +2,6 @@
 # usage: 
 # ./runme --file="cross_compile_ffmpeg_minimal.sh" --sandbox="ffmpeg" --xcomp="xc" --lib="dll or static"
 
-# Build FFmpeg as shared DLL
-
 PWD=$(pwd)
 
 # parse the command line
@@ -29,14 +27,8 @@ do
     ;;
   esac
 done
-if [ -z $FILE2RUN] || [ -z $SANDBOX ] || [ -z $XCOMP ] || [ -z $LIBTYPE ] ; then
+if [ -z $FILE2RUN ] || [ -z $SANDBOX ] || [ -z $XCOMP ] || [ -z $LIBTYPE ] ; then
     echo "error, must set directory names for file2run (--file=\"\"), sandbox (--sandbox=\"\"), cross-compiler (--xcomp=\"\") and library type (--libtype=\"\") ... exiting"
-  exit 1
-fi
-
-# does the ffmpeg-windows-build-helper file exist?
-if [ ! -e $FILE2RUN ] ; then
-  echo "error, file $FILE2RUN doesn't exist ... exiting"
   exit 1
 fi
 
@@ -56,12 +48,24 @@ else
   exit 1
 fi
 
+# deconstruct the name of the ffmpeg-windows-build-helper file 
+FILE2RUN_EXTN="${FILE2RUN##*.}"
+FILE2RUN_BASE="${FILE2RUN%.*}"
+echo "FILE2RUN_EXTN = $FILE2RUN_EXTN"
+echo "FILE2RUN_BASE = $FILE2RUN_BASE"
+
+# does the ffmpeg-windows-build-helper file exist?
+if [ ! -e $FILE2RUN ] ; then
+  echo "error, file $FILE2RUN doesn't exist ... exiting"
+  exit 1
+fi
+
 # cross compile 
 source ./$FILE2RUN --build-ffmpeg-static=$STATIC --build-ffmpeg-shared=$SHARED --enable-gpl=n --disable-nonfree=y --git-get-latest=n --prefer-stable=y --build-intel-qsv=y --build-dvbtee=n --build-mp4box=n --build-mplayer=n --build-vlc=n --build-lsw=n --build-x264-with-libav=n --ffmpeg-git-checkout-version= --build-ismindex=n --sandboxdir=$SANDBOX --xcompdir=$XCOMP 2>&1 | tee $DATFILE
 
 # get resultant bin files
 NOW=$(date +"%y%m%d-%H%M%S")
-BIN_DIR_NAME="$PWD/bin_files/$NOW_$SANDBOX_$XCOMP_$LIBTYPE_$FILE2RUN"
+BIN_DIR_NAME="${PWD}/bin_files/${NOW}_${LIBTYPE}_${FILE2RUN_BASE}"
 if [ ! -d "$BIN_DIR_NAME" ]; then
   mkdir -p "$BIN_DIR_NAME"
 else
